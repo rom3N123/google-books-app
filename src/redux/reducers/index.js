caed8b69782762c50abe5888ed7dce4dba322cfc
Roman <kseexy@mail.ru>
2021-09-04 14:53:08 +0300

    fixed export

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,3 @@
-export { default as fethStatus } from "./fetchStatusReducer";
+export { default as fetchStatus } from "./fetchStatusReducer";
 export { default as books } from "./booksReducer";
 export { default as query } from "./queryReducer";
