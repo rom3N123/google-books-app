e656bf13b043219644d3027dfc0470049dea6dcc
Roman <kseexy@mail.ru>
2021-09-04 15:13:05 +0300

    added axios interceptors

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
+const apiKey = "AIzaSyD93l_WjSrfAYINozVasESgW5T595q_0BA";
+
 const $api = axios.create({
    baseURL: "https://www.googleapis.com/books/v1/",
+});
+
+$api.interceptors.request.use((config) => {
+   if (config.url.includes("volumes?")) {
+      config.url += "&maxResults=30";
+   }
+
+   config.url += "&key=" + apiKey;
+
+   return config;
 });
 
 export default $api;
