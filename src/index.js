65e62b80136057aa613f1a3df492acaaab72337a
Roman <kseexy@mail.ru>
2021-09-04 11:54:37 +0300

    connected store to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import GlobalStyles from "./globalStyles";
+import store from "./redux/store";
 
 ReactDOM.render(
-   <>
+   <Provider store={store}>
       <GlobalStyles />
+
       <App />
-   </>,
+   </Provider>,
    document.getElementById("root")
 );
