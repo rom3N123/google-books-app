a1f8ac4d9fb279404d9f772d894e9bbc6d9b5c23
Roman <kseexy@mail.ru>
2021-09-04 17:24:11 +0300

    refactored search btn

--- src/components/Buttons/SearchBtn.jsx
+++ src/components/Buttons/SearchBtn.jsx
@@ -1,25 +1,11 @@
 import React from "react";
-import styled from "styled-components/macro";
 
-import searchIcon from "../../assets/search-icon.svg";
+import IconButton from "./IconButton";
 
-function SearchBtn({ ...props }) {
-   return (
-      <Button {...props}>
-         <Image src={searchIcon} alt="Search icon" />
-      </Button>
-   );
+import searchIconSvg from "../../assets/search-icon.svg";
+
+function SearchBtn() {
+   return <IconButton imageSrc={searchIconSvg} imageAlt={"Search icon"} />;
 }
 
 export default SearchBtn;
-
-const Button = styled.button`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-`;
-
-const Image = styled.img`
-   width: 24px;
-   height: 24px;
-`;
