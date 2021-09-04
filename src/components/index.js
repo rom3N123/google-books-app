a7210db74cc7af4ea1a24a387cf5fa93dffce64d
Roman <kseexy@mail.ru>
2021-09-04 11:54:56 +0300

    added exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Input } from "./Input";
 export { default as Container } from "./Container";
 export { default as Select } from "./Select";
 export { default as SearchBtn } from "./Buttons/SearchBtn.jsx";
+export { default as BookItem } from "./BookItem";
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,4 @@
 export { default as Header } from "../containers/Header";
 export { default as Search } from "../containers/Search";
+export { default as BooksList } from "./BooksList";
+export { default as BooksCatalog } from "./BooksCatalog";
