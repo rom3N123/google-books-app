cc63d42cc17607d91c61ff373feb39dbb9a75ec6
Roman <kseexy@mail.ru>
2021-09-04 17:14:18 +0300

    made book page info

--- src/components/BookPageInfo.jsx
+++ src/components/BookPageInfo.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-function BookPageInfo({ bookInfo }) {
+function BookPageInfo({ bookInfo, ...props }) {
    return (
-      <Wrapper>
+      <Wrapper {...props}>
          <Inner>
-            <Category>Computers</Category>
+            <Category>
+               {bookInfo.categories && bookInfo.categories.join("/")}
+            </Category>
 
-            <Title>Заголовок</Title>
+            <Title>{bookInfo.title}</Title>
 
-            <Authors>Авторы</Authors>
+            <Authors>{bookInfo.authors && bookInfo.authors.join(", ")}</Authors>
 
-            <Description>Описание</Description>
+            <Description>
+               {bookInfo.description && bookInfo.description}
+            </Description>
          </Inner>
       </Wrapper>
    );
@@ -24,6 +28,11 @@ const Wrapper = styled.div``;
 const Inner = styled.div`
    display: flex;
    flex-direction: column;
+   padding: 20px;
+
+   & > *:not(:last-child) {
+      margin-bottom: 20px;
+   }
 `;
 
 const Category = styled.p``;
@@ -32,4 +41,6 @@ const Title = styled.h3``;
 
 const Authors = styled.p``;
 
-const Description = styled.p``;
+const Description = styled.p`
+   line-height: 1.25;
+`;
