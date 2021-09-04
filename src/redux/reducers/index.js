fd23824cca87d9c628872b73e9bbaa25d64a118d
Roman <kseexy@mail.ru>
2021-09-04 16:42:19 +0300

    connected book page reducer to the project

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,4 @@
 export { default as fetchStatus } from "./fetchStatusReducer";
 export { default as books } from "./booksReducer";
 export { default as query } from "./queryReducer";
+export { default as book } from "./bookPageReducer";
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore } from "redux";
-import { books, fetchStatus, query } from "./reducers";
+import { books, fetchStatus, query, book } from "./reducers";
 
 const rootReducer = combineReducers({
    books,
    fetchStatus,
    query,
+   book,
 });
 
 const store = createStore(
