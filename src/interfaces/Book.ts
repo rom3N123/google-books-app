1fcc9e0c770caeb02393631285eeb04b4513e2e6
Roman <kseexy@mail.ru>
2021-09-05 12:04:18 +0300

    made book interface

---
+++ src/interfaces/Book.ts
@@ -0,0 +1,9 @@
+interface IBook {
+   thumbnail: string[] | null;
+   categories: string[] | null;
+   title: string;
+   author: string[] | null;
+   description: string | null;
+}
+
+export default IBook;
