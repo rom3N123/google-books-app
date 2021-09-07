d60248fe3d2b4817279b61b77fe428a8ce99bc02
Roman <kseexy@mail.ru>
2021-09-07 11:52:17 +0300

    refactored small button component

--- src/components/SmallButton.tsx
+++ src/components/SmallButton.tsx
@@ -2,22 +2,68 @@ import React from "react";
 import styled from "styled-components/macro";
 import { IconButton } from "@material-ui/core";
 
-function SmallButton({ children }: { children: React.ReactNode }) {
-   return <Button>{children}</Button>;
+interface ISmallButton {
+   backgroundColor?: string;
+   width?: string;
+   height?: string;
+   childrenColor?: string;
+   hoverBackgroundColor?: string;
+   hoverColor?: string;
+}
+
+function SmallButton({
+   children,
+   backgroundColor = "#fff",
+   width = "24px",
+   height = "24px",
+   childrenColor = "#262626",
+   hoverBackgroundColor = "#f5efef",
+   hoverColor = "none",
+}: ISmallButton & { children: React.ReactNode }) {
+   return (
+      <Button
+         width={width}
+         height={height}
+         backgroundColor={backgroundColor}
+         childrenColor={childrenColor}
+         hoverBackgroundColor={hoverBackgroundColor}
+         hoverColor={hoverColor}
+      >
+         {children}
+      </Button>
+   );
 }
 
 export default SmallButton;
 
 const Button = styled(IconButton)`
    && {
-      width: 24px;
-      height: 24px;
-      border-radius: 50%;
-      color: #262626;
-      background-color: #ffff;
-      font-size: 14px;
-      font-weight: 700;
-      box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-         rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+      ${({
+         backgroundColor,
+         childrenColor,
+         width,
+         height,
+         hoverBackgroundColor,
+         hoverColor,
+      }: ISmallButton) => `
+            width: ${width};
+            height: ${height};
+            border-radius: 50%;
+            color: #262626;
+            background-color: ${backgroundColor};
+            font-size: 14px;
+            font-weight: 700;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+               rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+               & > *:first-child {
+                  color: ${childrenColor};
+               }
+
+               &:hover {
+                  color: ${hoverColor};
+                  background-color: ${hoverBackgroundColor};
+               }
+         `}
    }
 `;
