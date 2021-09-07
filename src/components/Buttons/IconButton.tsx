f3a4e1c97d04e0d676888e47e57a67fa4e940618
Roman <kseexy@mail.ru>
2021-09-07 16:41:37 +0300

    refactored icon button

--- src/components/Buttons/IconButton.tsx
+++ src/components/Buttons/IconButton.tsx
@@ -1,32 +1,78 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { IconButton } from "@material-ui/core";
 
-interface IIconButton {
-   imageSrc: string;
-   imageAlt: string;
+interface ISmallButtonStyles {
+   backgroundColor?: string;
+   children: React.ReactNode;
+   width?: string;
+   height?: string;
+   childrenColor?: string;
+   hoverBackgroundColor?: string;
+   hoverColor?: string;
+   fontSize?: string;
+   type?: "button" | "submit" | undefined;
 }
 
-const IconButton: React.FC<IIconButton> = ({
-   imageSrc,
-   imageAlt,
-   ...props
-}) => {
+function SmallButton({
+   children,
+   backgroundColor = "transparent",
+   width = "24px",
+   height = "24px",
+   childrenColor = "#262626",
+   hoverBackgroundColor = "#f5efef",
+   hoverColor = "none",
+   onClick,
+   fontSize = "0.95rem",
+   type,
+}: ISmallButtonStyles & React.HTMLAttributes<HTMLButtonElement>) {
    return (
-      <Button {...props}>
-         <Image src={imageSrc} alt={imageAlt} />
+      <Button
+         onClick={onClick}
+         width={width}
+         height={height}
+         backgroundColor={backgroundColor}
+         childrenColor={childrenColor}
+         hoverBackgroundColor={hoverBackgroundColor}
+         hoverColor={hoverColor}
+         type={type}
+         fontSize={fontSize}
+      >
+         {children}
       </Button>
    );
-};
+}
 
-export default IconButton;
+export default SmallButton;
 
-const Button = styled.button`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-`;
+const Button = styled(IconButton)`
+   && {
+      ${({
+         backgroundColor,
+         childrenColor,
+         width,
+         height,
+         hoverBackgroundColor,
+         hoverColor,
+         fontSize,
+      }: ISmallButtonStyles) => `
+            width: ${width};
+            height: ${height};
+            border-radius: 50%;
+            color: #262626;
+            background-color: ${backgroundColor};
+            font-weight: 600;
+            font-size: ${fontSize};
+            transition: all .2s ease;
+               & > *:first-child {
+                  color: ${childrenColor};
+                  
+               }
 
-const Image = styled.img`
-   width: 24px;
-   height: 24px;
+               &:hover {
+                  color: ${hoverColor};
+                  background-color: ${hoverBackgroundColor};
+               }
+         `}
+   }
 `;
