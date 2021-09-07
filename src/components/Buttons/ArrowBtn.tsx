2d19dd4c8fe7f6ae76b37b5b1e09dcabc76919ea
Roman <kseexy@mail.ru>
2021-09-07 11:52:30 +0300

    made arrow button component

---
+++ src/components/Buttons/ArrowBtn.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from "react";
+import SmallButton from "../SmallButton";
+
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+
+interface IArrowBtnProps {
+   direction?: "left" | "right";
+}
+
+function ArrowBtn({
+   direction = "left",
+   ...props
+}: IArrowBtnProps & React.HTMLAttributes<HTMLDivElement>): ReactElement {
+   return (
+      <SmallButton
+         childrenColor="#fff"
+         backgroundColor="#262626"
+         hoverBackgroundColor="#555454"
+         {...props}
+      >
+         {direction === "left" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      </SmallButton>
+   );
+}
+
+export default ArrowBtn;
